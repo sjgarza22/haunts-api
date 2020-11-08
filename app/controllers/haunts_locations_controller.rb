9bcc751a3fa8ce6a58aedf80f68d134cd85031bb
sjgarza22 <sjgarza22@gmail.com>
2020-11-08 01:43:38 -0600

    Add route methods and private params method

---
+++ app/controllers/haunts_locations_controller.rb
@@ -0,0 +1,15 @@
+class HauntsLocationsController < ApplicationController
+    def show
+
+    end
+
+    def update
+        
+    end
+
+    private
+
+    def haunts_location_params
+        params(:haunts_location)
+    end
+end
---
+++ app/controllers/ratings_controller.rb
@@ -0,0 +1,15 @@
+class RatingsController < ApplicationController
+    def show
+        
+    end
+
+    def update
+
+    end
+
+    private
+
+    def rating_params
+
+    end
+end
