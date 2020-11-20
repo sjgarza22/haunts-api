5eea7d479a9d70f808a372dea67be9195c95db10
sjgarza22 <sjgarza22@gmail.com>
2020-11-19 18:48:25 -0600

    Add Skip_before_action for index/show. Filled out new, create, show and destroy methods. Finished haunt_params method.

--- app/controllers/haunts_controller.rb
+++ app/controllers/haunts_controller.rb
@@ -1,24 +1,49 @@
-class HauntsController < ApplicatonControler
+class HauntsController < ApplicationController
+    skip_before_action :authorized, only: [:index, :show]
+
     def index
         haunts = Haunt.all
-        render json: HauntSerializer.new(haunts).to_serialized_json
+        render json: HauntSerializer.new(haunts).serialized_json
+    end
+
+    def new
+        haunt = Haunt.new
     end
 
     def create
+        haunt = Haunt.create(name: haunt_params[:name], description: haunt_params[:description])
+        haunt.create_haunts_location(city: haunt_params[:city], state: haunt_params[:state])
 
+        if haunt.valid?
+            render json: HauntSerializer.new(haunt, haunt.haunts_location).serialized_json, status: :accepted
+        else
+            render json: { message: 'Could not save new Haunt' }, status: :unaccepted
+        end
     end
 
     def show
+        haunt = Haunt.find_by_id(params['id'])
 
+        if haunt
+            render json: HauntSerializer.new(haunt).serialized_json, status: :accepted
+        else
+            render json: { message: 'Could not find Haunt' }, status: :not_found
+        end
     end
 
     def destroy
+        haunt = Haunt.find_by_id(params['id'])
 
+        if haunt.destroy
+            render json: HauntSerializer.new(haunt, haunts_location).serialized_json, status: :accepted
+        else
+            render json: { message: 'Could not find Haunt' }, status: :not_found
+        end
     end
 
     private
 
     def haunt_params
-
+        params.require(:haunts).permit(:name, :description, :city, :state, :address)
     end
 end
