651aae8a84346116598bacc9168c0695f9923ed5
sjgarza22 <sjgarza22@gmail.com>
2020-11-22 01:01:53 -0600

    Created and setup Geocoder configure file.

---
+++ config/initializers/geocoder.rb
@@ -0,0 +1,23 @@
+# config/initializers/geocoder.rb
+Geocoder.configure(
+
+    # street address geocoding service (default :nominatim)
+    lookup: :pickpoint,
+  
+    # IP address geocoding service (default :ipinfo_io)
+    ip_lookup: :maxmind,
+  
+    # to use an API key:
+    api_key: ENV['SECRET_KEY'],
+  
+    # geocoding service request timeout, in seconds (default 3):
+    timeout: 5,
+  
+    # set default units to miles:
+    units: :mi
+  
+    # caching (see [below](#caching) for details):
+    # cache: Redis.new,
+    # cache_prefix: "..."
+  
+  )
