83ae7f385a78dd8bb8a833272c870347a4ccf2d1
sjgarza22 <sjgarza22@gmail.com>
2020-11-22 00:13:03 -0600

    Added private search params, use params to search Near the location provided. Test results and render to Json if they pass.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,16 +3,21 @@ class SearchController < ApplicationController
     skip_before_action :authorized, only: [:create]
 
     def create
-        searchInput = params[:search][:search_input]      
-        results = HauntsLocation.all
+        searchInput = search_params[:search_input]
+        searchArea = search_params[:search_area]
+        # byebug
+        results = HauntsLocation.near(searchInput, searchArea)
 
-        puts results
-        if results
+        if results && results.length > 0
             render json: HauntsLocationSerializer.new(results).serialized_json, status: :accepted
         else
             render json: { message: 'No locations found' }, status: :not_found
         end
     end
 
-    
+    private
+
+    def search_params
+        params.require(:search).permit(:search_input, :search_area)
+    end
 end
