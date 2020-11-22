26364bea1bd41e5c7f0c2b9af1c8ab94b43940f0
sjgarza22 <sjgarza22@gmail.com>
2020-11-22 00:52:39 -0600

    For nested route haunts/haunts_location removed update. Added route for search.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
       resources :haunts do
-        resources :haunts_locations, only: [:show, :update]
+        resources :haunts_locations, only: [:show]
       end
 
       resources :ratings
@@ -12,4 +12,7 @@ Rails.application.routes.draw do
 
       resources :users, only: [:create]
       post '/login', to: 'auth#create'
+
+      resources :haunts, only: [:create]
+      post '/search', to: 'search#create'
 end
