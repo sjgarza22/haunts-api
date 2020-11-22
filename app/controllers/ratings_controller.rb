fd670d87cb475a178f8382573ed3a41c15271ced
sjgarza22 <sjgarza22@gmail.com>
2020-11-22 01:27:53 -0600

    Finished Update method. Added private check_new_rating method to validate new rating.

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -15,7 +15,7 @@ class RatingsController < ApplicationController
     end
     
     def show
-        rating = Rating.find_by_id(params['id'])
+        rating = Rating.find_by_id(rating_params['id'])
         
         if rating
             render json: RatingSerializer.new(rating).serialized_json, status: :found
@@ -25,12 +25,27 @@ class RatingsController < ApplicationController
     end
 
     def update
+        newRating = Rating.find_by_id(rating_params['id'])
 
+        if newRating && check_new_rating(rating_params['rating'])
+            newRating.rating = rating_params['rating']
+            render json: RatingSerializer.new(newRating).serialized_json, status: :update
+        else
+            render json: { message: 'rating not updated' }, status: :not_updated
+        end
     end
 
     private
 
     def rating_params
-        params.require(:rating).permit(:rating, :haunt_id, :user_id)
+        params.require(:rating).permit(:id, :rating, :haunt_id, :user_id)
+    end
+
+    def check_new_rating(rating)
+        if rating >= 1 && rating <= 5
+            return true
+        else
+            return false
+        end
     end
 end
