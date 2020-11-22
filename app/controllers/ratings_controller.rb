3e5ee5cc79376ce9c0b9905b8e5d8d5b7f04cb47
sjgarza22 <sjgarza22@gmail.com>
2020-11-22 00:03:18 -0600

    Finished new, create and show methods.

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,14 +1,27 @@
 class RatingsController < ApplicationController
-    def new
 
+    def new
+        rating = Rating.new
     end
 
     def create
+        rating = Rating.create(rating_params)
 
+        if rating.valid?
+            render json: RatingSerializer.new(rating).serialized_json, status: :accepted
+        else
+            render json: { message: 'rating not created' }, status: :not_acceptable
+        end
     end
     
     def show
+        rating = Rating.find_by_id(params['id'])
         
+        if rating
+            render json: RatingSerializer.new(rating).serialized_json, status: :found
+        else
+            render json: { message: 'rating not found' }, status: :not_found
+        end
     end
 
     def update
@@ -18,6 +31,6 @@ class RatingsController < ApplicationController
     private
 
     def rating_params
-
+        params.require(:rating).permit(:rating, :haunt_id, :user_id)
     end
 end
