d998e708abc7f4536a15d415f11fe2bb588bdd0e
sjgarza22 <sjgarza22@gmail.com>
2020-12-01 21:58:07 -0600

    Removed dependent/destory from has_one relationship

--- app/models/haunt.rb
+++ app/models/haunt.rb
@@ -1,7 +1,7 @@
 class Haunt < ApplicationRecord
     # extend Geocoder::Model::ActiveRecord
     
-    has_one :haunts_location, :dependent => :destroy
+    has_one :haunts_location
     has_many :ratings
 
     validates :name, presence: true, uniqueness: true
