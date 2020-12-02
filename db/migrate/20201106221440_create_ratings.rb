0fd653069e339456aa2547d7d71ba2cfc52ca865
sjgarza22 <sjgarza22@gmail.com>
2020-12-01 22:02:54 -0600

    Fixed references from haunts to haunt

--- db/migrate/20201106221440_create_ratings.rb
+++ db/migrate/20201106221440_create_ratings.rb
@@ -2,7 +2,7 @@ class CreateRatings < ActiveRecord::Migration[6.0]
   def change
     create_table :ratings do |t|
       t.integer :rating
-      t.references :haunts, null: false, foreign_key: true
+      t.references :haunt, null: false, foreign_key: true
 
       t.timestamps
     end
