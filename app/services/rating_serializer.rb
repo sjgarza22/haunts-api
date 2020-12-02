751c49538af08a62acf1ed99ee0b431c3faf5203
sjgarza22 <sjgarza22@gmail.com>
2020-12-01 21:59:29 -0600

    Added rating to attributes

--- app/services/rating_serializer.rb
+++ app/services/rating_serializer.rb
@@ -1,6 +1,6 @@
 class RatingSerializer
     include FastJsonapi::ObjectSerializer
-    attributes :id, :haunt_id, :user_id
+    attributes :id, :rating, :haunt_id, :user_id
     
     belongs_to :haunt
     belongs_to :user
